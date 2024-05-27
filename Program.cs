277a7c6ff984bb541fc131c4c9069fbc0b71723a
BoraEfe <Bora.e@icloud.com>
2024-05-27 12:48:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Spotify
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean kevin = true;
+            if (kevin == true)
+            {
+                Console.WriteLine("Kevin is the best!");
+            }
+        }
+    }
+}
